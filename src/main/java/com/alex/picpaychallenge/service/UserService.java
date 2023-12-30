ccb0e7c0f36d2d1df36ec7759b2d399298a84ad4
Alex Santana <alequeixam@gmail.com>
2023-12-29 23:54:51 -0300

    add few new methods

--- src/main/java/com/alex/picpaychallenge/service/UserService.java
+++ src/main/java/com/alex/picpaychallenge/service/UserService.java
@@ -1,11 +1,13 @@
 package com.alex.picpaychallenge.service;
 
+import com.alex.picpaychallenge.domain.user.TypeOfUser;
 import com.alex.picpaychallenge.domain.user.User;
 import com.alex.picpaychallenge.domain.user.dto.UserDTO;
 import com.alex.picpaychallenge.domain.user.dto.UserResponse;
 import com.alex.picpaychallenge.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Service
@@ -16,6 +18,9 @@ public class UserService {
         this.repository = repository;
     }
 
+    public void saveUser(User user) {
+        repository.save(user);
+    }
     public UserResponse createUser(UserDTO userDTO) {
         var user = new User(userDTO);
         repository.save(user);
@@ -26,7 +31,13 @@ public class UserService {
         var user = repository.findById(id)
                 .orElseThrow(() -> new RuntimeException("User not found."));
 
-        user = new User(userDTO);
+        user.setId(id);
+        user.setName(userDTO.name());
+        user.setDocument(userDTO.document());
+        user.setEmail(userDTO.email());
+        user.setPassword(userDTO.password());
+        user.setBalance(userDTO.balance());
+        user.setTypeOfUser(userDTO.typeOfUser());
         repository.save(user);
 
         return mapToResponse(user);
@@ -35,6 +46,11 @@ public class UserService {
     public List<UserResponse> getAllUsers() {
         var users = repository.findAll();
         return users.stream().map(user -> mapToResponse(user)).toList();
+    }
+
+    public User findUserById(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User does not exist in the database."));
     }
 
     private UserDTO mapToDTO(User user) {
@@ -54,5 +70,17 @@ public class UserService {
     private UserResponse mapToResponse (User user) {
         return new UserResponse(user.getId(), user.getName(), user.getDocument(), user.getEmail(),
                 user.getTypeOfUser(), user.getBalance());
+    }
+
+    public boolean validateUser(User payee, BigDecimal value) {
+
+        if (payee.getTypeOfUser() != TypeOfUser.COMMON) {
+            throw new RuntimeException("Merchants can not make transactions.");
+        }
+
+        if (payee.getBalance().compareTo(value) < 0) {
+            throw new RuntimeException("Insufficient balance on this account");
+        }
+        return true;
     }
 }
